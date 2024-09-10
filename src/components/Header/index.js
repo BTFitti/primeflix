ec8a5eadaed8251cc683a2916ac3e92919fbfc95
BTFitti <bruno.fitti734@live.com>
2024-09-10 12:05:28 -0300

    Iniciando projeto PrimeFlix

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <span>Primeflix</span>
+      <a className="botao">Meus filmes</a>
+    </header>
+  );
+}
+export default Header;
---
+++ src/pages/Filme/index.js
@@ -0,0 +1,8 @@
+function Filme(){
+    return(
+        <div>
+            <h1>Pagina de detalhes do filme.</h1>
+        </div>
+    )
+}
+export default Filme;
